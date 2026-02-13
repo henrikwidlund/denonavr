245bb36ecb18d9711bf6deabb44680a8b785de68
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-02-13 18:24:57 +0100

    Linting

--- denonavr/api.py
+++ denonavr/api.py
@@ -559,6 +559,25 @@ class DenonAVRTelnetApi:
     )
     _update_callback_tasks: Set[asyncio.Task] = attr.ib(default=attr.Factory(set))
 
+    # Keep track of the following events since they are sent
+    # when manually requesting updates. The receiver will send them back,
+    # even if they've not changed.
+    _potential_duplicate_events: dict[str, str] = attr.ib(
+        init=False,
+        factory=lambda: {
+            "INFSIGRES I": "",
+            "INFSIGRES O": "",
+            "SDVIN ": "",
+            "SDVOUT ": "",
+            "INFSIGHDR I": "",
+            "INFSIGHDR O": "",
+            "INFSIGPIX": "",
+            "INFSIGFRL I": "",
+            "INFSIGFRL O": "",
+            "INFSIGCOS": "",
+        },
+    )
+
     def __attrs_post_init__(self) -> None:
         """Initialize special attributes."""
         self._register_raw_callback(self._send_confirmation_callback)
@@ -864,21 +883,6 @@ class DenonAVRTelnetApi:
         """Unregister a callback handler for raw telnet messages."""
         self._raw_callbacks.remove(callback)
 
-    # Keep track of the following events since they are sent when manually requesting updates.
-    # The receiver will send them back, even if they've not changed.
-    _potential_duplicate_events: dict[str, str] = {
-        "INFSIGRES I": "",
-        "INFSIGRES O": "",
-        "SDVIN ": "",
-        "SDVOUT ": "",
-        "INFSIGHDR I": "",
-        "INFSIGHDR O": "",
-        "INFSIGPIX": "",
-        "INFSIGFRL I": "",
-        "INFSIGFRL O": "",
-        "INFSIGCOS": "",
-    }
-
     def _process_event(self, message: str) -> None:
         """Process a realtime event."""
         _LOGGER.debug("Incoming Telnet message: %s", message)
@@ -919,14 +923,15 @@ class DenonAVRTelnetApi:
             return
 
         # For events that are sent when manually requesting updates,
-        # check if the parameter is the same as the last one and ignore if so to prevent triggering callbacks twice.
+        # check if the parameter is the same as the last one and ignore
+        # if so to prevent triggering callbacks twice.
         # Update the last parameter for the event otherwise.
         for _i, key in enumerate(self._potential_duplicate_events.keys()):
             if parameter.startswith(key):
                 current_value = self._potential_duplicate_events[key]
                 if parameter == current_value:
                     _LOGGER.debug(
-                        "Duplicate message received for event %s with parameter %s, ignoring",
+                        "Ignoring duplicate event %s with parameter %s",
                         event,
                         parameter,
                     )
