98566b286efab12496ef623eda498db8bcb5ea09
ol-iver <scarface@mywoh.de>
2026-07-06 22:05:53 +0200

    Create new dev version 1.4.0-dev

--- denonavr/__init__.py
+++ denonavr/__init__.py
@@ -17,7 +17,7 @@ from .ssdp import async_identify_denonavr_receivers
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __title__ = "denonavr"
-__version__ = "1.3.4-dev"
+__version__ = "1.4.0-dev"
 
 
 async def async_discover():
